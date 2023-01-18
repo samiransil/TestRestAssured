67721c66a56c1e22bc9a543b897a43dced2a28fa
Samiran Sil <122539843+samiransil@users.noreply.github.com>
2023-01-18 01:28:19 +0000

    Update get method

--- RestAPIDemo/src/test/java/FunctionalTesting/GetRequestAutomation.java
+++ RestAPIDemo/src/test/java/FunctionalTesting/GetRequestAutomation.java
@@ -1,6 +1,7 @@
 package FunctionalTesting;
 
 import javax.annotation.meta.When;
+import javax.xml.crypto.Data;
 
 import org.testng.annotations.Test;
 import io.restassured.RestAssured;
@@ -12,11 +13,27 @@ public class GetRequestAutomation {
 	
 	@Test
 	public void getRequestAutomation() {
-		RestAssured.baseURI = "https://dummy.restapiexample.com/api/v1/employees";
-		Response response = RestAssured.given().param("id", "1").when().get();
-		System.out.println("getStatusCode:-" + response.getStatusCode());
-		System.out.println("Samiran Sil");
-		System.out.println(response.then().assertThat().log().all().statusCode(200));
+//		
+//		RestAssured.baseURI = "https://dummy.restapiexample.com/api/v1/employees/9570";
+//		Response response = RestAssured.given().param("id", "9309").when().get();
+//		System.out.println("getStatusCode:-" + response.getStatusCode());
+//		System.out.println("Samiran Sil");
+//		System.out.println(response.then().assertThat().log()
+//				.all()
+//				.statusCode(200));
+		
+		
+		
+		Response response = RestAssured.get("https://dummy.restapiexample.com/api/v1/employees");
+		System.out.println(response.getStatusCode());
+		System.out.println(response.getBody().asPrettyString());
+		System.out.println("Samiran");
+		
+		Response response1 = RestAssured.get("https://dummy.restapiexample.com/api/v1/employee/1");
+		System.out.println(response1.getStatusCode());
+		System.out.println(response1.getBody().asPrettyString());
+		System.out.println("Sil");
+		
 	}
 	
 }
